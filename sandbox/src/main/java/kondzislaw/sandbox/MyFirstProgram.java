4c254b66bfbcea9f34f8620f3ee6bf23d5d16b88
Konrad_Zajaczkowski <45081457+Kondzislaw@users.noreply.github.com>
2018-11-18 19:42:39 +0100

    Add some new functions

--- sandbox/src/main/java/kondzislaw/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/kondzislaw/sandbox/MyFirstProgram.java
@@ -3,7 +3,30 @@ package kondzislaw.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Powierzchnia prostokąta o bokach "  + a +  " i " + b + " = " + area(a, b));
+
+    double len = 5;
+    System.out.println("Powierzchnia kwadratu o boku " + len + " = " + area(len));
+
+  }
+
+  public static void hello(String somebody) {
+       System.out.println("Hello," + somebody + "!");
+
+  }
+
+  public static double area(double l) {
+    return l * l;
+
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+
   }
 
 }
