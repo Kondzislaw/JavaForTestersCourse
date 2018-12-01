fbab466a90481a8f1afbe06e74b38a5ad1e878a3
Konrad_Zajaczkowski <45081457+Kondzislaw@users.noreply.github.com>
2018-12-01 20:24:18 +0100

    Final solution for exercise 9

--- addressbook-web-tests/src/test/java/kondzislaw/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/kondzislaw/addressbook/tests/ContactCreationTest.java
@@ -8,8 +8,6 @@ public class ContactCreationTest extends TestBase{
 
   @Test
   public void testContactCreation() throws Exception {
-    //app.getContactsHelper().fillContactForm(new ContactData("Konrad", "Tester", "Test Street\nTest City\n01-111 Test", "555444333", "konrad@test.com", "Test1UPDATED"), true);
-    //app.getContactsHelper().submitContactCreation();
     app.getContactsHelper().createContact(new ContactData("Konrad", "Tester", "Test Street\nTest City\n01-111 Test", "555444333", "konrad@test.com", "Test1UPDATED"), true);
     app.getNavigationHelper().gotoHomePage();
     app.getSessionHelper().logOut();
