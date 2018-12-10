9eb6d5fda209a8fc6cc0197086cc4b79f3c1229f
Konrad_Zajaczkowski <45081457+Kondzislaw@users.noreply.github.com>
2018-12-10 20:16:52 +0100

    getID for groups

--- addressbook-web-tests/src/test/java/kondzislaw/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/kondzislaw/addressbook/model/GroupData.java
@@ -3,14 +3,28 @@ package kondzislaw.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String Name, String Header, String Footer) {
+    this.id = null;
     name = Name;
     header = Header;
     footer = Footer;
+  }
+
+  public GroupData(String id, String Name, String Header, String Footer) {
+    this.id = id;
+    name = Name;
+    header = Header;
+    footer = Footer;
+  }
+
+  public String getId() {
+    return id;
   }
 
   public String getName() {
@@ -26,22 +40,25 @@ public class GroupData {
   }
 
   @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
-  }
-
-  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
+    return Objects.equals(id, groupData.id) &&
+            Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(name);
+    return Objects.hash(id, name);
   }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
+
 }
--- addressbook-web-tests/src/test/java/kondzislaw/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/kondzislaw/addressbook/tests/GroupModificationTest.java
@@ -4,6 +4,7 @@ import kondzislaw.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -18,11 +19,18 @@ public class GroupModificationTest extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().editGroup();
-    app.getGroupHelper().fillGroupForm(new GroupData("Test1UPDATED", "TestUPUP2", "Test3UPDATED"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"Test1UPDATED", "TestUPUP2", "Test3UPDATED");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().updateGroup();
     app.getGroupHelper().returnToGroupPage("groups");
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+
+    before.remove(before.size() -1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
     app.getSessionHelper().logOut();
 
   }
