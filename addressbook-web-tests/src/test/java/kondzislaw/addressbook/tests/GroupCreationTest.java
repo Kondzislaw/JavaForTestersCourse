8a63e51adfa93810856a42a00cebd7a241820636
Konrad_Zajaczkowski <45081457+Kondzislaw@users.noreply.github.com>
2018-12-11 20:32:06 +0100

    Max ID by lambda

--- addressbook-web-tests/src/test/java/kondzislaw/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/kondzislaw/addressbook/tests/GroupCreationTest.java
@@ -16,7 +16,7 @@ public class GroupCreationTest extends TestBase {
 
     app.getNavigationHelper().gotoGroupPage("groups");
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
@@ -29,7 +29,7 @@ public class GroupCreationTest extends TestBase {
       }
     }
 
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
